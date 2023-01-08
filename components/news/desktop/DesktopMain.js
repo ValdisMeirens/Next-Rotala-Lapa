e6cd4a11da7a298f04617c832158da87afe45403
Valdis Meirens <valdismeirens@gmail.com>
2023-01-09 00:33:00 +0200

    Valdis Happy To make smooth image backgroudn changing

--- components/news/desktop/DesktopMain.js
+++ components/news/desktop/DesktopMain.js
@@ -1,108 +1,14 @@
-import { useState, useEffect, useRef } from "react";
-import Image from "next/image";
-import { AnimateSharedLayout, LayoutGroup, motion } from "framer-motion";
+import { LayoutGroup } from "framer-motion";
 import classes from "./DesktopMain.module.css";
 import DesktopMainDiv from "./DesktopMainDiv.js";
 
 const DesktopMain = (props) => {
-  // const firstNews = (
-  //   <DesktopMainDiv
-  //     title={props.news.data.news[0].title}
-  //     smallText={props.news.data.news[0].smallText}
-  //     image={props.news.data.news[0].image}
-  //     ID={props.news.data.news[0].ID}
-  //     text={props.news.data.news[0].text}
-  //   />
-  // );
-
-  // const secondNews = (
-  //   <DesktopMainDiv
-  //     title={props.news.data.news[1].title}
-  //     smallText={props.news.data.news[1].smallText}
-  //     image={props.news.data.news[1].image}
-  //     ID={props.news.data.news[1].ID}
-  //     text={props.news.data.news[1].text}
-  //   />
-  // );
-
-  // const thirdNews = (
-  //   <DesktopMainDiv
-  //     title={props.news.data.news[2].title}
-  //     smallText={props.news.data.news[2].smallText}
-  //     image={props.news.data.news[2].image}
-  //     ID={props.news.data.news[2].ID}
-  //     text={props.news.data.news[2].text}
-  //   />
-  // );
-
-  // const [first, setFirst] = useState(true);
-  // const [second, setSecond] = useState(false);
-  // const [third, setThird] = useState(false);
-
-  const arrayLenght = props.news.data.news.length - 1;
-  let counter = 0;
-
-  const [title, setTitle] = useState(props.news.data.news[0].title);
-  const [smallText, setSmallText] = useState(props.news.data.news[0].smalltext);
-  const [picture, setPicture] = useState(props.news.data.news[0].image);
-  const [newsid, setNewsId] = useState(props.news.data.news[0].ID);
-  const [text, setText] = useState(props.news.data.news[0].text);
-
-  useEffect(() => {
-    const changeNews = setInterval(() => {
-      counter = counter === arrayLenght ? 0 : counter + 1;
-      setTitle(props.news.data.news[counter].title);
-      setSmallText(props.news.data.news[counter].smalltext);
-      setPicture(props.news.data.news[counter].image);
-
-      setNewsId(props.news.data.news[counter].ID);
-      setText(props.news.data.news[counter].text);
-    }, 3000);
-    return () => clearInterval(changeNews);
-  }, []);
-
-  // useEffect(() => {
-  //   const changeNews = setInterval(() => {
-  //     console.log("esmu te");
-
-  //     if (first) {
-  //       console.log("first" + first);
-  //       setFirst(false);
-  //       setSecond(true);
-  //     } else if (second) {
-  //       console.log("second" + second);
-
-  //       setSecond(false);
-  //       setThird(true);
-  //     } else if (third) {
-  //       console.log("third" + third);
-
-  //       setThird(false);
-  //       setFirst(true);
-  //     }
-  //   }, 3000);
-  //   return () => clearInterval(changeNews);
-  // }, [first, second, third]);
-
   return (
-    // <div className={classes.desktopContainer}>
-    //   <LayoutGroup>
-    //     {first && firstNews}
-    //     {second && secondNews}
-    //     {third && thirdNews}
-    //   </LayoutGroup>
-    // </div>
-    <LayoutGroup>
-      <motion.div className={classes.desktopContainer}>
-        <DesktopMainDiv
-          title={title}
-          smallText={smallText}
-          image={picture}
-          ID={newsid}
-          text={text}
-        />
-      </motion.div>
-    </LayoutGroup>
+    <div className={classes.desktopContainer}>
+      <LayoutGroup>
+        <DesktopMainDiv data={props} />
+      </LayoutGroup>
+    </div>
   );
 };
 
--- components/news/desktop/DesktopMainDiv.js
+++ components/news/desktop/DesktopMainDiv.js
@@ -1,33 +1,61 @@
 import classes from "./DesktopMainDiv.module.css";
 import DesktopNewsContent from "./DesktopNewsContent.js";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useCycle } from "framer-motion";
+import { useEffect } from "react";
 
-const DesktopMainDiv = ({ title, smallText, image, ID, text }) => {
+import new2 from "../../../public/aboutus/Donats.png";
+
+const animationVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visiable: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const DesktopMainDiv = (props) => {
+  const newsArray = props.data.news.data.news.map((news) => (
+    <motion.div
+      variants={animationVariant}
+      key={news.ID}
+      initial="hidden"
+      animate="visiable"
+      exit="exit"
+    >
+      <Image src={new2} alt="TDA Rotaļa jaunumi" className={classes.image} />
+      <DesktopNewsContent
+        title={news.title}
+        smallText={news.smallText}
+        newsid={news.ID}
+        text={news.text}
+      />
+    </motion.div>
+  ));
+
+  const [currentImage, setCurrentImage] = useCycle(...newsArray);
+
+  useEffect(() => {
+    const timeout = setInterval(setCurrentImage, 5000);
+
+    return () => clearInterval(timeout);
+  }, [setCurrentImage, currentImage]);
+
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <motion.div>
-          <Image
-            src={`/news/${image}`}
-            alt="TDA Rotaļa jaunumi"
-            className={classes.image}
-            width={2500}
-            height={2500}
-          />
-        </motion.div>
-        <DesktopNewsContent
-          title={title}
-          smallText={smallText}
-          newsid={ID}
-          text={text}
-        />
-      </motion.div>
-    </AnimatePresence>
+    <div className={classes.container}>
+      <AnimatePresence>{currentImage}</AnimatePresence>
+    </div>
   );
 };
 
